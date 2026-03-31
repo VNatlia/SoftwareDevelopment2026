214570143d03610526694f62a31ca6f8f0b889ee
nataliia <nvolynets371@gmail.com>
2026-03-31 21:30:05 +0300

    some changes

--- Lab2TaskTextt/src/main/java/org/softwareDevelopment/Main.java
+++ Lab2TaskTextt/src/main/java/org/softwareDevelopment/Main.java
@@ -1,6 +1,8 @@
 package org.softwareDevelopment;
 
 import java.util.Random;
+import java.util.Scanner;
+
 
 
 
@@ -43,5 +45,44 @@ public class Main {
         }
 
         System.out.println("Сума:" + sum);
+
+        // ===== ЗАВДАННЯ 2 =====
+        Scanner scanner = new Scanner(System.in);
+
+        String text = "Думи мої думи тяжко мені з вами";
+
+        System.out.println("\nТекст до обробки:");
+        System.out.println(text);
+
+        System.out.print("\nВвести довжину слова щоб видалити: ");
+        int length = scanner.nextInt();
+
+        String prigolosni = "бвгґджзйклмнпрстфхцчшщ";
+
+        String[] words = text.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            String cleanWord = word.replaceAll("[^а-яА-ЯіїєІЇЄa-zA-Z]", "");
+
+            if (cleanWord.length() == length) {
+                char firstChar = Character.toLowerCase(cleanWord.charAt(0));
+
+                if (prigolosni.indexOf(firstChar) != -1) {
+                    continue;
+                }
+            }
+
+            result.append(word).append(" ");
+        }
+
+        System.out.println("\nТекст після обробки:");
+        System.out.println(result.toString().trim());
+
+        System.out.println("\nВхідні дані:");
+        System.out.println("Довжина слова: " + length);
     }
-}
+
+    }
+
